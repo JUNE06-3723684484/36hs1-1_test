505bde063facf3714bf0db49be4ef14d440eda09
원준 <92675559+JUNE06-3723684484@users.noreply.github.com>
2022-06-28 16:04:12 +0900

    Update form.php

--- form.php
+++ form.php
@@ -1,6 +1,6 @@
 <?php
 $nametext = array($_POST['email'], " / ", $_POST['text']);
 
-	file_put_contents('database/'.$_POST['name'], $nametext);
-	header('Location: /contact.html');
+	file_put_contents('./database/'.$_POST['name'], $nametext);
+	header('Location: ./contact.html');
 ?>
